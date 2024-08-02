c3f0c25fd3f66a6a11fcf458e01dc83bb96cd73f
Azanuddin <azdin.bpc@gmail.com>
2024-08-02 19:49:08 +0700

    Update gradle misc

--- app/src/main/java/com/example/belajarmobileapps/MainActivity.java
+++ app/src/main/java/com/example/belajarmobileapps/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.belajarmobileapps;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button bproses;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bproses = (Button) findViewById(R.id.proses);
+        bproses.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Ini Adalah Contoh Toas", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
